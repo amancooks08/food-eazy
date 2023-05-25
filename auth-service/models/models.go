7a7160bbc45bf3baaea90dc0a89a088efcfe0e20
amandeep <amandeep.chhabra_ftc@external.swiggy.in>
2023-05-25 15:20:31 +0530

    Add tests and code for RegisterUser model

--- auth-service/models/models.go
+++ auth-service/models/models.go
@@ -1,15 +1,20 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"auth-service/errors"
+	logger "github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
 
 var db *gorm.DB
 
 type User struct {
 	gorm.Model
-	Name string 			`gorm:"column:name; unique; primarykey; AUTO_INCREMENT;"`
-	Email string 			`gorm:"column:email; unique; not null"`
-	Password string 		`gorm:"not null"`
-	PhoneNumber string 		`gorm:"unique; not null"`
+	ID 		    uint   `gorm:"primaryKey; autoIncrement; not null"`
+	Name        string `gorm:"column:name;"`
+	Email       string `gorm:"column:email; unique; not null"`
+	Password    string `gorm:"column:password; not null"`
+	PhoneNumber string `gorm:"column:phoneNumber; unique; not null"`
 }
 
 func InitAuthModels(database *gorm.DB) {
@@ -17,3 +22,13 @@ func InitAuthModels(database *gorm.DB) {
 	db.AutoMigrate(&User{})
 }
 
+func RegisterUser(user *User) error {
+	if user == nil {
+		return errors.ErrInvalidUser
+	}
+	if err := db.Create(user).Error; err != nil {
+		logger.WithField("error", err).Error(errors.ErrCreateUser.Error())
+		return errors.ErrCreateUser
+	}
+	return nil
+}
--- auth-service/models/models_test.go
+++ auth-service/models/models_test.go
@@ -3,6 +3,8 @@ package models
 import (
 	"testing"
 
+	"auth-service/errors"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"gorm.io/driver/sqlite"
@@ -16,9 +18,7 @@ type AuthModelsTestSuite struct {
 
 func (suite *AuthModelsTestSuite) SetupSuite() {
 	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
-	if err != nil {
-		suite.FailNow("failed to connect database")
-	}
+	assert.NoError(suite.T(), err)
 	suite.db = db
 	InitAuthModels(db)
 }
@@ -41,6 +41,70 @@ func (suite *AuthModelsTestSuite) TestUserModelInitialized() {
 	})
 }
 
-func (suite *AuthModelsTestSuite) TestUserModel() {
-	
+func (suite *AuthModelsTestSuite) TestModels_RegisterUser() {
+	t := suite.T()
+
+	t.Run("Register user with valid details", func(t *testing.T) {
+		testUser := User{
+			Name:        "test",
+			Email:       "test1@gmail.com",
+			Password:    "test123",
+			PhoneNumber: "1234567890",
+		}
+
+		err := RegisterUser(&testUser)
+		assert.NoError(t, err)
+	})
+
+	t.Run("Register user with nil user", func(t *testing.T) {
+		err := RegisterUser(nil)
+		assert.Error(t, err)
+		assert.Equal(t, err.Error(), errors.ErrInvalidUser.Error())
+	})
+
+	t.Run("Register user with duplicate email", func(t *testing.T) {
+		testUser := User{
+			Name:        "test2",
+			Email:       "test2@mail.com",
+			Password:    "test123",
+			PhoneNumber: "1234567891",
+		}
+
+		err1 := RegisterUser(&testUser)
+		assert.NoError(t, err1)
+
+		duplicateUser := User{
+			Name:        "test3",
+			Email:       "test2@mail.com",
+			Password:    "test123",
+			PhoneNumber: "1234567893",
+		}
+
+		err2 := RegisterUser(&duplicateUser)
+		assert.Error(t, err2)
+		assert.Equal(t, err2.Error(), errors.ErrCreateUser.Error())
+	})
+
+	t.Run("Register user with duplicate phone number", func(t *testing.T) {
+		testUser := User{
+			Name:        "test3",
+			Email:       "test3@mailcom",
+			Password:    "test123",
+			PhoneNumber: "1234567893",
+		}
+
+		err1 := RegisterUser(&testUser)
+		assert.NoError(t, err1)
+
+		duplicateUser := User{
+			Name:        "test4",
+			Email:       "test4@mail.com",
+			Password:    "test123",
+			PhoneNumber: "1234567893",
+		}
+
+		err2 := RegisterUser(&duplicateUser)
+		assert.Error(t, err2)
+		assert.Equal(t, err2.Error(), errors.ErrCreateUser.Error())
+	})
 }
