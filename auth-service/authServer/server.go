d0cee723e18e7e70419005c32f8741a5a230b86f
amandeep <amandeep.chhabra_ftc@external.swiggy.in>
2023-05-26 15:59:33 +0530

    implement grpc server

--- auth-service/authServer/server.go
+++ auth-service/authServer/server.go
@@ -1,9 +1,47 @@
 package authServer
 
-import(
+import (
 	"context"
 
 	proto "auth-service/proto/authpb"
-
-	
+	"auth-service/service"
 )
+
+type GRPCServer struct {
+	proto.UnimplementedAuthServiceServer
+}
+
+func (s *GRPCServer) RegisterUser(ctx context.Context, req *proto.RegisterUserRequest) (*proto.RegisterUserResponse, error) {
+	err := service.RegisterUser(req.Name, req.Email, req.Password, req.PhoneNumber, req.Role.String())
+	if err != nil {
+		return nil, err
+	}
+
+	return &proto.RegisterUserResponse{
+		Message: "User registered successfully",
+	}, nil
+}
+
+func (s *GRPCServer) LoginUser(ctx context.Context, req *proto.LoginUserRequest) (*proto.LoginUserResponse, error) {
+	token, err := service.LoginUser(req.Email, req.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	return &proto.LoginUserResponse{
+		Token: token,
+	}, nil
+}
+
+func (s *GRPCServer) ValidateUser(ctx context.Context, req *proto.ValidateTokenRequest) (*proto.ValidateTokenResponse, error) {
+	status, err := service.ValidateUser(req.Token)
+	if err != nil {
+		return nil, err
+	}
+
+	return &proto.ValidateTokenResponse{
+		StatusCode: int32(status),
+		Message:    "Token is valid",
+	}, nil
+}
+
