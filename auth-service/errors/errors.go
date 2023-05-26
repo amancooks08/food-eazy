26539e36e8032a8255163aad998c38ef1f562735
amandeep <amandeep.chhabra_ftc@external.swiggy.in>
2023-05-26 07:36:21 +0530

    Add error messages

--- auth-service/errors/errors.go
+++ auth-service/errors/errors.go
@@ -9,12 +9,16 @@ var (
 	ErrFailedToConnectDB = errors.New("failed to connect to database")
 	ErrFailedToCloseDB = errors.New("failed to close database")
 	
+	ErrUserNotFound = errors.New("user not found")
 	ErrInvalidUser = errors.New("invalid user")
+	ErrCreateUser = errors.New("failed to create user")
 	ErrInvalidPhoneNumber = errors.New("invalid phone number")
 	ErrInvalidEmail = errors.New("invalid email")
 	ErrDuplicateEmail = errors.New("email already exists")
-	ErrShortPassword = errors.New("password must be atleast 8 characters long")
 	ErrEmptyField = errors.New("empty field")
 
-	ErrCreateUser = errors.New("failed to create user")
+	ErrInvalidPassword = errors.New("invalid password")
+	ErrHashPassword	= errors.New("failed to hash password")
+	ErrShortPassword = errors.New("password must be atleast 8 characters long")
+
 )
