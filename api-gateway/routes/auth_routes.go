f1d6303d79e8836864697b895fc7bd942825780c
amandeep <amandeep.chhabra_ftc@external.swiggy.in>
2023-05-28 16:28:52 +0530

    init auth routes

---
+++ api-gateway/routes/auth_routes.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"api-gateway/handlers/authHandlers"
+	proto "api-gateway/proto/auth"
+	"github.com/gorilla/mux"
+)
+
+func InitAuthRoutes(router *mux.Router, authService proto.AuthServiceClient) {
+	router.HandleFunc("/register", authHandlers.RegisterUser(authService)).Methods("POST")
+}
