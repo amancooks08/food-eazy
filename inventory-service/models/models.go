2c7f12edd7baec96858c54b11b9098ce893db644
amandeep <amandeep.chhabra_ftc@external.swiggy.in>
2023-05-29 18:40:06 +0530

    add tests and code for models to add item, get item and get all item

--- inventory-service/models/models.go
+++ inventory-service/models/models.go
@@ -1,16 +1,21 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"inventory-service/errors"
+
+	logger "github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
 
 var db *gorm.DB
 
 type Item struct {
 	gorm.Model
-	ID          uint   `gorm:"primaryKey; column:id; autoIncrement; not null"`
-	Name        string `gorm:"column:name; not null"`
-	Description string `gorm:"column:description; not null"`
-	Price       float64   `gorm:"column:price; not null"`
-	Quantity    uint   `gorm:"column:quantity; not null"`
+	ID          uint    `gorm:"primaryKey; column:id; autoIncrement; not null"`
+	Name        string  `gorm:"column:name; unique; not null"`
+	Description string  `gorm:"column:description; not null"`
+	Price       float64 `gorm:"column:price; not null"`
+	Quantity    uint    `gorm:"column:quantity; not null"`
 }
 
 func InitInventoryModels(database *gorm.DB) {
@@ -18,10 +23,34 @@ func InitInventoryModels(database *gorm.DB) {
 	db.AutoMigrate(&Item{})
 }
 
-func CreateItem(item Item) error {
-	err := db.Create(&item).Error
-	if err != nil {
-		return err
+func CreateItem(item *Item) (*Item, error) {
+	if item == nil {
+		logger.WithField("error", errors.ErrInvalidItem.Error()).Error(errors.ErrInvalidItem.Error())
+		return nil, errors.ErrInvalidItem
 	}
-	return nil
+	err := db.Create(item).Error
+	if err != nil {
+		logger.WithField("error", err.Error).Error(errors.ErrAddItem.Error())
+		return nil, errors.ErrAddItem
+	}
+	return item, nil
+}
+
+func GetItem(id uint) (*Item, error) {
+	item := &Item{}
+	err := db.Where("id = ?", id).First(item).Error
+	if err != nil {
+		return nil, err
+	}
+	return item, nil
+}
+
+
+func GetAllItems() ([]*Item, error) {
+	items := []*Item{}
+	err := db.Find(&items).Error
+	if err != nil {
+		return nil, err
+	}
+	return items, nil
 }
--- inventory-service/models/models_test.go
+++ inventory-service/models/models_test.go
@@ -43,14 +43,114 @@ func (suite *InventoryModelsTestSuite) TestModels_AddItem() {
 	t := suite.T()
 
 	t.Run("Add item successfully", func(t *testing.T) {
-		testItem := Item{
+		testItem := &Item{
 			Name:        "testitem1",
 			Description: "testitem1.desc",
 			Price:       100,
 			Quantity:    100,
 		}
 
-		err := CreateItem(testItem)
+		item, err := CreateItem(testItem)
 		assert.NoError(t, err)
+		assert.NotNil(t, item)
+		assert.Equal(t, testItem.Name, item.Name)
+		assert.Equal(t, testItem.Description, item.Description)
+		assert.Equal(t, testItem.Price, item.Price)
+		assert.Equal(t, testItem.Quantity, item.Quantity)
+	})
+
+	t.Run("Add item with nil value", func(t *testing.T) {
+		item, err := CreateItem(nil)
+		assert.Error(t, err)
+		assert.Nil(t, item)
+	})
+
+	t.Run("Add item with duplicate name", func(t *testing.T) {
+		testItem1 := &Item{
+			Name:        "testitem2",
+			Description: "testitem2.desc",
+			Price:       100,
+			Quantity:    100,
+		}
+
+		item, err := CreateItem(testItem1)
+		assert.NoError(t, err)
+		assert.NotNil(t, item)
+
+		testItem2 := &Item{
+			Name:        "testitem2",
+			Description: "testitem2.desc",
+			Price:       100,
+			Quantity:    100,
+		}
+
+		item, err = CreateItem(testItem2)
+		assert.Error(t, err)
+		assert.Nil(t, item)
+	})
+}
+
+func (suite *InventoryModelsTestSuite) TestModels_GetItem() {
+	t := suite.T()
+
+	t.Run("Get item successfully", func(t *testing.T) {
+		testItem := &Item{
+			Name:        "testitem3",
+			Description: "testitem3.desc",
+			Price:       100,
+			Quantity:    10,
+		}
+
+		item, err := CreateItem(testItem)
+		assert.NoError(t, err)
+		assert.NotNil(t, item)
+
+		got, err := GetItem(item.ID)
+		assert.NoError(t, err)
+		assert.NotNil(t, got)
+		assert.Equal(t, testItem.Name, item.Name)
+		assert.Equal(t, testItem.Description, item.Description)
+		assert.Equal(t, testItem.Price, item.Price)
+		assert.Equal(t, testItem.Quantity, item.Quantity)
+	})
+
+	t.Run("Get item with invalid id", func(t *testing.T) {
+		got, err := GetItem(0)
+		assert.Error(t, err)
+		assert.Nil(t, got)
+	})
+}
+
+
+func (suite *InventoryModelsTestSuite) TestModels_GetAllItems() {
+	t := suite.T()
+
+	t.Run("Get all items successfully", func(t *testing.T) {
+		testItem1 := &Item{
+			Name:        "testitem4",
+			Description: "testitem4.desc",
+			Price:       100,
+			Quantity:    10,
+		}
+
+		item1, err := CreateItem(testItem1)
+		assert.NoError(t, err)
+		assert.NotNil(t, item1)
+
+		testItem2 := &Item{
+			Name:        "testitem5",
+			Description: "testitem5.desc",
+			Price:       100,
+			Quantity:    10,
+		}
+
+		item2, err := CreateItem(testItem2)
+		assert.NoError(t, err)
+		assert.NotNil(t, item2)
+
+		got, err := GetAllItems()
+		assert.NoError(t, err)
+		assert.NotNil(t, got)
+		assert.Equal(t, 4, len(got))
 	})
 }
