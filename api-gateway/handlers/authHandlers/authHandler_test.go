e1b38b09899098ba8e7b92f837933333b757df0e
amandeep <amandeep.chhabra_ftc@external.swiggy.in>
2023-05-29 10:56:13 +0530

    add test for auth handler

--- api-gateway/handlers/authHandlers/authHandler_test.go
+++ api-gateway/handlers/authHandlers/authHandler_test.go
@@ -88,5 +88,48 @@ func (suite *AuthHandlerTestSuite) TestRegisterUser() {
 		// Arrange
 		requestBody := domain.RegisterUserRequest{
 			Name:        "test1",
-			Email:       "
+			Email:       "testting@mailcom",
+			Password:   "test@1234",
+			PhoneNumber: "9876543987",
+			Role:        "USER",
+		}
+
+		expectedRequest := proto.RegisterUserRequest{
+			Name:        requestBody.Name,
+			Email:       requestBody.Email,
+			Password:    requestBody.Password,
+			PhoneNumber: requestBody.PhoneNumber,
+			Role:        domain.RoleMap[requestBody.Role],
+		}
+
+		expectedResponse := proto.RegisterUserResponse{
+			StatusCode: http.StatusBadRequest,
+			Message: "invalid request body",
+		}
+
+		resp := domain.Message{
+			Message: expectedResponse.Message,
+		}
+
+		exp, err := json.Marshal(resp)
+		if err != nil {
+			t.Errorf("error while marshalling expected response: %v", err)
+		}
+
+		jsonRequest := `{"name" : "test1", "email" : "testting@mailcom", "password" : "test@1234", "phone_number" : "9876543987" , "role":"USER"}`
+		req := httptest.NewRequest("POST", "/register", strings.NewReader(jsonRequest))
+		res := httptest.NewRecorder()
+
+		// Act
+
+		suite.grpc.On("RegisterUser", context.Background(), &expectedRequest).Return(&expectedResponse, nil).Once()
+		deps := &dependencies.Dependencies{
+			AuthService: suite.grpc,
+		}
+		// Assert
+		handler := RegisterUser(deps.AuthService)
+		handler.ServeHTTP(res, req)
+		assert.Equal(t, http.StatusBadRequest, res.Code)
+		assert.Equal(t, string(exp), res.Body.String())
+	})
 }
