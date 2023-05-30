1862f15aa5e1c8f65a45c64d455818853e93d572
amandeep <amandeep.chhabra_ftc@external.swiggy.in>
2023-05-30 10:55:32 +0530

    add test for PlaceORder service

--- order-service/service/orderservice_test.go
+++ order-service/service/orderservice_test.go
@@ -1,8 +1,11 @@
 package service
 
 import (
+	"context"
 	"net/http"
+	mocks "order-service/mocks/inventoryMocks"
 	"order-service/models"
+	proto "order-service/proto/inventorypb"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,7 +16,8 @@ import (
 
 type OrderServiceTestSuite struct {
 	suite.Suite
-	db *gorm.DB
+	db         *gorm.DB
+	mockclient *mocks.InventoryServiceClient
 }
 
 func (suite *OrderServiceTestSuite) SetupSuite() {
@@ -40,6 +44,48 @@ func (suite *OrderServiceTestSuite) TestService_PlaceOrder() {
 
 	t.Run("PlaceOrder successfully", func(t *testing.T) {
 		//Arrange
-		
+		itemID, userID := uint32(1), uint32(1)
+		quantity, amount := uint32(2), float32(1.0)
 
+		itemResponse := &proto.GetItemResponse{
+			StatusCode:  http.StatusOK,
+			Id:          itemID,
+			Name:        "item",
+			Description: "item description",
+			Price:       10,
+			Quantity:    12,
+		}
+
+		order := &models.Order{
+			UserID:    userID,
+			ItemID:    itemID,
+			Quantity:  quantity,
+			Amount:    amount,
+			OrderTime: "2021-01-01 00:00:00",
+		}
+
+		//Act
+		suite.mockclient.On("GetItem", context.Background(), &proto.GetItemRequest{
+			Id: itemID,
+		}).Return(itemResponse, nil).Once()
+
+		suite.mockclient.On("LowerQuantity", context.Background(), &proto.LowerQuantityRequest{
+			Id:       itemID,
+			Quantity: quantity}).Return(&proto.LowerQuantityResponse{
+			StatusCode: http.StatusOK,
+			Id:         1,
+			Quantity:   10}, nil).Once()
+
+		status, placedOrder, err := PlaceOrder(userID, itemID, quantity)
+
+		//Assert
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusCreated, status)
+		assert.Equal(t, order.ID, placedOrder.ID)
+		assert.Equal(t, order.UserID, placedOrder.UserID)
+		assert.Equal(t, order.ItemID, placedOrder.ItemID)
+		assert.Equal(t, order.Quantity, placedOrder.Quantity)
+		assert.Equal(t, order.Amount, placedOrder.Amount)
+		assert.Equal(t, order.OrderTime, placedOrder.OrderTime)
+	})
 }
