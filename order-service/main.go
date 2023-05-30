05b18cc0b84a6ac159336f20b21b951c1f383964
amandeep <amandeep.chhabra_ftc@external.swiggy.in>
2023-05-30 14:40:13 +0530

    add grpc server for order service

---
+++ order-service/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "order-service/inventoryClient"
+
+func main() {
+	inventoryClient.InitGRPCClient()
+}
--- order-service/orderServer/server.go
+++ order-service/orderServer/server.go
@@ -4,17 +4,30 @@ import (
 	"context"
 
 	proto "order-service/proto/orderpb"
+	client "order-service/proto/inventorypb"
 	"order-service/service"
 )
 
 type GRPCServer struct {
+	*service.OrderService
 	proto.UnimplementedOrderServiceServer
 }
 
+func NewGRPCServer(Client client.InventoryServiceClient) *GRPCServer {
+	service := service.NewOrderService(Client)
+	return &GRPCServer{
+		service,
+		proto.UnimplementedOrderServiceServer{},
+	}
+}
+
 func (s *GRPCServer) PlaceOrder(ctx context.Context, req *proto.PlaceOrderRequest) (*proto.PlaceOrderResponse, error) {
-	statusCode, order, err := service.PlaceOrder(req.UserId, req.ItemId, req.Quantity)
+	statusCode, order, err := s.OrderService.PlaceOrder(req.UserId, req.ItemId, req.Quantity)
 	if err != nil {
-		return nil, err
+		return &proto.PlaceOrderResponse{
+			StatusCode: statusCode,
+			Order:      nil,
+		}, err
 	}
 
 	return &proto.PlaceOrderResponse{
