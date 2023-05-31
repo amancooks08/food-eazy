edef91bb2e02d90d7d1920d734af2e2af142eabb
amandeep <amandeep.chhabra_ftc@external.swiggy.in>
2023-05-31 17:21:32 +0530

    intitalize order service grpc server

--- order-service/main.go
+++ order-service/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "order-service/inventoryClient"
+import (
+	"net"
+	"order-service/config"
+	"order-service/database"
+	"order-service/inventoryClient"
+	"order-service/proto/orderpb"
+	logger "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"order-service/orderServer"
+)
 
 func main() {
 	inventoryClient.InitGRPCClient()
+	config.Load()
+
+	if err := database.InitDB(); err != nil {
+		logger.WithField("error", err).Error("Error connecting to database")
+		return
+	}
+	defer database.Close()
+
+	gRPCServer := grpc.NewServer()
+
+	orderpb.RegisterOrderServiceServer(gRPCServer, orderServer.NewGRPCServer(inventoryClient.InventoryServiceClient))
+
+	logger.Info("Starting gRPC server on port 33003")
+	lis, err := net.Listen("tcp", ":33003")
+	if err != nil {
+		logger.WithField("error", err).Error("Error starting inventory gRPC server")
+		return
+	}
+
+	if err := gRPCServer.Serve(lis); err != nil {
+		logger.WithField("error", err).Error("Error starting inventory gRPC server")
+		return
+	}
+
+	defer lis.Close()
+	defer gRPCServer.Stop()
 }
